4224e56e3c6ea65242b114c0ad914cd24263057a
kamil.jedrzejuk <kamil.jedrzejuk@calamari.io>
2024-10-01 18:49:31 +0200

    "Add test for creating a new account (failing)"

---
+++ src/test/kotlin/AccountServiceTest.kt
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+class AccountServiceTest {
+
+    @Test
+    fun `should create a new account with valid details`() {
+        // given
+        val name = "Jan Kowalski"
+        val initialBalance = 1000.0
+
+        // when
+        val account = accountService.create(name, initialBalance);
+
+        // then
+        assertNotNull(account.id, "Account ID should not be null")
+        assertEquals(initialBalance, account.balancePln, "Account balance in PLN should match initial balance")
+        assertEquals(name, account.owner, "Account owner name should match provided name")
+    }
+}

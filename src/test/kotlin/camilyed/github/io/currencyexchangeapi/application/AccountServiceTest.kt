3f97f6fbc58ae0763473446cc119200d6a635620
kamil.jedrzejuk <kamil.jedrzejuk@calamari.io>
2024-10-01 21:02:49 +0200

    Add test for creating a new account with negative balance (failing)"

--- src/test/kotlin/camilyed/github/io/currencyexchangeapi/application/AccountServiceTest.kt
+++ src/test/kotlin/camilyed/github/io/currencyexchangeapi/application/AccountServiceTest.kt
@@ -1,8 +1,8 @@
 package camilyed.github.io.currencyexchangeapi.application
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.math.BigDecimal
 
 class AccountServiceTest {
@@ -16,11 +16,26 @@ class AccountServiceTest {
         val initialBalance = BigDecimal(1000.0)
 
         // when
-        val account = accountService.create(CreateAccountCommand(owner =  name, initialBalance = initialBalance))
+        val account = accountService.create(CreateAccountCommand(owner = name, initialBalance = initialBalance))
 
         // then
         assertNotNull(account.id, "Account ID should not be null")
         assertEquals(initialBalance, account.balancePln, "Account balance in PLN should match initial balance")
         assertEquals(name, account.owner, "Account owner name should match provided name")
+    }
+
+    @Test
+    fun `should throw exception when trying to create account with negative balance`() {
+        // given
+        val name = "Jan Kowalski"
+        val initialBalance = BigDecimal(1000.0)
+
+        // when
+        val exception = assertThrows<IllegalArgumentException> {
+            accountService.create(CreateAccountCommand(owner = name, initialBalance = initialBalance))
+        }
+
+        // and
+        assertEquals("Initial balance cannot be negative", exception.message)
     }
 }

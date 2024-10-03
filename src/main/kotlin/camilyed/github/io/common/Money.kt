c07fb22ee7a28c2271c6f4b7a479b17d6ea3f024
kamil.jedrzejuk <kamil.jedrzejuk@calamari.io>
2024-10-03 10:46:20 +0200

    Implement test for adding money

--- src/main/kotlin/camilyed/github/io/common/Money.kt
+++ src/main/kotlin/camilyed/github/io/common/Money.kt
@@ -3,8 +3,14 @@ package camilyed.github.io.common
 import java.math.BigDecimal
 
 data class Money(val amount: BigDecimal, val currency: String) {
+
     init {
         require(amount >= BigDecimal.ZERO) { "Money amount must be greater than or equal to zero" }
         require(currency.isNotEmpty()) { "Currency cannot be empty" }
+    }
+
+    operator fun plus(other: Money): Money {
+        require(currency == other.currency) { "Currencies must match to perform addition" }
+        return Money(amount.add(other.amount), currency)
     }
 }
